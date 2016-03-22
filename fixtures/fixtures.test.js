36f6e1d54b7e9afde2ce273daa3c0038cd2a5a1b
Kiril Vatev <vatev.1@gmail.com>
2016-03-21 22:11:09 -0400

    updating README, fixtures

--- fixtures/fixtures.test.js
+++ fixtures/fixtures.test.js
@@ -3,23 +3,23 @@
 var util = require('util');
 var lipsum = require('lorem-ipsum');
 
+var times = [2, 4, 8, 16, 32, 64, 128, 256, 512];
+
 describe('[slow tests]', function() {
-    
-    var times = [2, 4, 8, 16, 32, 64, 128, 256, 512];
-    
     times.forEach(function(t) {
         it(t + 'ms', function(done) {
             setTimeout(done, t);
         });
     });
-    
-    describe('long name', function() {
-        times.forEach(function(t) {
-            var name = util.format('%dms %s', t, lipsum({ count: Math.max(5, t/10), units: 'words' }));
-            
-            it(name, function(done) {
-                setTimeout(done, t);
-            });
+});
+
+
+describe('[slow test long name]', function() {
+    times.forEach(function(t) {
+        var name = util.format('%dms %s', t, lipsum({ count: Math.max(5, t/10), units: 'words' }));
+
+        it(name, function(done) {
+            setTimeout(done, t);
         });
     });
 });
